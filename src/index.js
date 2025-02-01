b9662fb9ed9c5d139c3d5e3939cc2a0f50fabac9
Vinod Kumar <vinodrao835@gmail.com>
2025-02-01 11:31:12 +0530

    FIX: reverse proxy deprecation with express-proxy-middleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const { rateLimit } = require('express-rate-limit');
 
-
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const proxy = require('express-http-proxy');
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
@@ -15,20 +14,16 @@ const limiter = rateLimit({
     message: { error: 'Too many requests, please try again later.' },
 });
 
-const flightServiceProxyMiddleware = createProxyMiddleware({
-    target: ServerConfig.FLIGHT_SERVICE,
-    changeOrigin: true,
-});
-
-const bookingServiceProxyMiddleware = createProxyMiddleware({
-    target: ServerConfig.BOOKING_SERVICE,
-    changeOrigin: true,
-});
 
 app.use(limiter);
-app.use('/flightsService', flightServiceProxyMiddleware);
-app.use("/bookingService", bookingServiceProxyMiddleware);
 
+app.use('/flightsService', proxy(ServerConfig.FLIGHT_SERVICE, {
+    proxyReqPathResolver: (req) => req.originalUrl.replace('/flightsService', ''),
+}));
+
+app.use('/bookingService', proxy(ServerConfig.BOOKING_SERVICE, {
+    proxyReqPathResolver: (req) => req.originalUrl.replace('/bookingService', ''),
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
