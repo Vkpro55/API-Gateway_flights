d1b5c7676e75ced756b02ead0699e7ae443b4aed
Vinod Kumar <vinodrao835@gmail.com>
2025-02-01 11:22:06 +0530

    Added Reverse Proxy for API Gateway

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const { rateLimit } = require('express-rate-limit');
 
+
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 
@@ -12,7 +15,20 @@ const limiter = rateLimit({
     message: { error: 'Too many requests, please try again later.' },
 });
 
+const flightServiceProxyMiddleware = createProxyMiddleware({
+    target: ServerConfig.FLIGHT_SERVICE,
+    changeOrigin: true,
+});
+
+const bookingServiceProxyMiddleware = createProxyMiddleware({
+    target: ServerConfig.BOOKING_SERVICE,
+    changeOrigin: true,
+});
+
 app.use(limiter);
+app.use('/flightsService', flightServiceProxyMiddleware);
+app.use("/bookingService", bookingServiceProxyMiddleware);
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,3 +38,11 @@ app.use('/api', apiRoutes);
 app.listen(ServerConfig.PORT, () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
 });
+
+/**
+ * user
+ *  |
+ *  localhost:5000 => API Gateway
+ *  |
+ * localhost:3000/api/v1/flights
+ */
